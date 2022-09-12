81e3807d13f5ef9cb72541654d9c475480405aa6
Ahmed Ali <101448218+3ZMTOO@users.noreply.github.com>
2022-09-12 13:05:10 +0200

    Add files via upload
    assignment 7

---
+++ index.js
@@ -0,0 +1,130 @@
+//Q1 
+// Write a program that allow to user enter number then print it
+
+// var num = Number(prompt(`Enter a Number please :`));
+
+// console.log(num);
+
+//Q2
+// Write a program that take number from user then print yes if that number can divide by 3 
+// and 4 otherwise print no
+// var num = Number(prompt(`please Enter a number :`));
+// if(num%3==0 && num%4==0 )
+// {
+//     console.log("yes");
+// }
+// else
+// {
+//     console.log("no");
+// }
+
+//Q3
+// Write a program that allows the user to insert 2 integers then print the max
+
+// var num1 = Number(prompt(`Enter the first number:`));
+// var num2 = Number(prompt(`Enter the second number:`));
+// if (num1>num2)
+// {
+//     console.log(`{${num1} > ${num2}`);
+
+// }
+// else
+// {
+//         console.log(`${num1} < ${num2}`);
+// }
+
+
+
+//Q4 Write a program that allows the user to insert an integer then print negative if it is
+// negative number otherwise print positive
+
+// var num1 = Number(prompt(`Enter number:`));
+// if (num1>0)
+// {
+//     console.log(`${num1} positive`);
+    
+// }
+// else
+// {
+//     console.log(`${num1} negative`);
+
+// }
+
+
+// Write a program that take 3 integers from user then print the max element 
+// and the min element.
+
+// var num1 = Number(prompt(`Enter number1:`));
+// var num2 = Number(prompt(`Enter number2:`));
+// var num3 = Number(prompt(`Enter number3:`));
+
+// function max(num1, num2, num3) {
+//     if (num1>num2&&num1>num3)
+//     {
+//         console.log(`${num1} max`);
+//     }
+//     else if(num2>num1&&num2>num3)
+//     {
+//         console.log(`${num2} max`);
+
+//     }
+//     else
+//     {
+//         console.log(`${num3} max`)
+//     }
+// }
+// function min(num1, num2, num3) {
+//     if (num1<num2&&num1<num3)
+//     {
+//         console.log(`${num1} min`);
+//     }
+//     else if(num2<num1&&num2<num3)
+//     {
+//         console.log(`${num2} min`);
+
+//     }
+//     else
+//     {
+//         console.log(`${num3} min`)
+//     }
+
+// }
+// max(num1, num2, num3);
+// min(num1, num2, num3);
+
+// Write a program that allows the user to insert integer number then 
+// check If a number is oven or odd
+
+// var num1 = Number(prompt(`Enter number:`));
+// if (num1 %2 ==0)
+// {
+//     console.log(`even`);
+    
+// }
+// else
+// {
+//     console.log(`odd`);
+
+// }
+
+// Write a program that take character from user then if it is vowel chars (a,e,I,o,u) 
+// then print vowel otherwise print consonant
+
+
+// var char = prompt(`Enter character:`);
+// if  (char == 'a' || char =='i'  || char == 'e' || char == 'o' || char == 'u')
+// {
+//     console.log(`vowel`);
+
+// }
+
+// else
+// {
+//     console.log(`consonant`);
+
+
+// }
+
+    
+
+
